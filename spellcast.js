44b9d27dc78dfb6023b18695b7462e06e8b23b7b
AlmostGalactic <143559054+AlmostGalactic@users.noreply.github.com>
2025-04-20 15:57:17 -0500

    Create spellcast.js

---
+++ spellcast.js
@@ -0,0 +1,8 @@
+// spellcast.js - Main module that imports and exports the classes
+
+import { Lexer } from './lexer.js';
+import { Parser } from './parser.js';
+import { Interpreter } from './interpreter.js';
+import { Environment } from './environment.js';
+
+export { Lexer, Parser, Interpreter, Environment };
